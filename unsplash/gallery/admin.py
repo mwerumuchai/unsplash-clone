5e2ff7e873e4c75e677fd7e1117274e9bc1c5c34
mwerumuchai <mwerumuchai@gmail.com>
2017-11-19 09:43:27 +0300

    tags in each photo

--- unsplash/gallery/admin.py
+++ unsplash/gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models  import User, Gallery, Tags
+
+class UnsplashAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Gallery,UnsplashAdmin)
+admin.site.register(Tags)
