53999c890fa155a1217a656e0d8198c7c714e9be
mwerumuchai <mwerumuchai@gmail.com>
2017-11-19 09:42:11 +0300

    add image settings to the app

--- unsplash/unsplash/settings.py
+++ unsplash/unsplash/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -85,6 +86,10 @@ DATABASES = {
     }
 }
 
+
+# Images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
