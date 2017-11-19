88af823b333a620e9e72005cd9420382d0ffd2ef
mwerumuchai <mwerumuchai@gmail.com>
2017-11-19 09:41:12 +0300

    create a view function for the photos

--- unsplash/gallery/views.py
+++ unsplash/gallery/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
 import datetime as dt
-
-
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def new_photos(request):
-    date = dt.date.today()
-
-    return render(request, 'all-gallery/new.html', {"date": date,})
+from .models import Gallery
 
 # trending photos
 def trending_photos(request,past_date):
@@ -23,4 +15,26 @@ def trending_photos(request,past_date):
     if date == dt.date.today():
         return redirect(new_photos)
 
-    return render(request, 'all-gallery/trending.html', {"date": date})
+    gallery = Gallery.trending_now(date)
+    return render(request, 'all-gallery/trending.html', {"date": date,})
+
+# new photos
+def new_photos(request):
+    date = dt.date.today()
+
+
+    gallery = Gallery.get_images
+    return render(request, 'all-gallery/new.html', {"date": date, "gallery": gallery, })
+
+# search photos
+def search_results(request):
+
+    if 'gallery' in request.GET and request.GET["gallery"]:
+        search_term = request.GET.get("gallery")
+        searched_gallery = Gallery.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gallery/search.html',{"message":message,"gallery": searched_gallery})
+    else:
+        message = "No photos searched"
+        return render(request, 'all-gallery/search.html',{"message":message})
