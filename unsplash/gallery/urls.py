318d16f69085d0b4e70608bf093bfd8ea8169c46
mwerumuchai <mwerumuchai@gmail.com>
2017-11-19 09:40:38 +0300

    add url static pattern

--- unsplash/gallery/urls.py
+++ unsplash/gallery/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', views.welcome,name = 'welcome'),
-    url('^new/$', views.new_photos,name = 'newPhotos'),
-    url(r'^trending/(\d{4}-\d{2}-\d{2})/$',views.trending_photos,name = 'trendingPhotos')
+    url(r'^$', views.new_photos,name = 'newPhotos'),
+    url(r'^trending/(\d{4}-\d{2}-\d{2})/$',views.trending_photos,name = 'trendingPhotos'),
+    url(r'^search/', views.search_results, name = 'search_results')
+    # url(r'^photo/(\d+)',views.article,name='photo')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
