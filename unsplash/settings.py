a0edc18734ef14e22018015876a322f677256eb1
mwerumuchai <mwerumuchai@gmail.com>
2017-11-21 11:24:50 +0300

    navigation bar: add more_vert

--- unsplash/settings.py
+++ unsplash/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['unsplash-clone2.herokuapp.com']
+ALLOWED_HOSTS = ['unsplash-clone2.herokuapp.com','127.0.0.1']
 
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
