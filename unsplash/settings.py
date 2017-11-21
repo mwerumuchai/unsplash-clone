b2eff73c4b1ff0becdd45091c5294ed32eb7ff6d
mwerumuchai <mwerumuchai@gmail.com>
2017-11-21 08:26:58 +0300

    deploy to heroku

--- unsplash/settings.py
+++ unsplash/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db from env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
