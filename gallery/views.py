e278ef1ff5a3d1dae562bc1bea3d86cfd2984e67
mwerumuchai <mwerumuchai@gmail.com>
2017-12-07 02:03:55 +0300

    create a single photo view function that gets a single photo that's' been celected and renders it in the single_photo html file

--- gallery/views.py
+++ gallery/views.py
@@ -25,7 +25,18 @@ def new_photos(request):
 
     gallery = Gallery.get_images
     tags = Tags.display_tags()
+    title = 'New Photos'
     return render(request, 'all-gallery/new.html', {"date": date, "gallery": gallery,"tags":tags })
+
+def single_photo(request,gallery_id):
+    try:
+        gallery = Gallery.objects.get(pk=gallery_id)
+        tags = Tags.display_tags()
+    except Post.DoesNotExist:
+        raise Http404("The Post does not exist")
+
+    return render(request,'all-gallery/single_photo.html',{"gallery":gallery, "tags":tags})
+
 
 # tags
 def tags(request,tag_id):
@@ -34,6 +45,7 @@ def tags(request,tag_id):
         gallery = Gallery.objects.filter(tags=tags).all()
     except DoesNotExist:
         raise Http404()
+    title = 'Tags'
     return render(request,'all-gallery/tag.html', {"tags": tags, "gallery":gallery})
 
 # search photos
@@ -48,4 +60,10 @@ def search_results(request):
         return render(request, 'all-gallery/search.html',{"message":message,"searched_tags": searched_tags, "gallery":gallery})
     else:
         message = "No photos searched"
+        title = 'Search Results'
         return render(request, 'all-gallery/search.html',{"message":message})
+
+def collections(request):
+    collected_tags = Tags.display_tags()
+    title = 'Collections'
+    return render(request,'all-gallery/collections.html', {"tags":collected_tags})
