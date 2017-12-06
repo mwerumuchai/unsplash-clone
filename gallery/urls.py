18531fd0ddce8d225854b24492767549b904f1a3
mwerumuchai <mwerumuchai@gmail.com>
2017-12-07 02:05:07 +0300

    add collections url path...

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r'^trending/(\d{4}-\d{2}-\d{2})/$',views.trending_photos,name = 'trendingPhotos'),
     url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^tag/(\d+)', views.tags, name = 'tag'),
-    url(r'^gallery/(\d+)',views.new_photos,name='gallery')
+    url(r'^gallery/(\d+)/$',views.single_photo,name='gallery'),
+    url( r'^collections/', views.collections, name="collections")
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
